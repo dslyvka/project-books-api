71b2fbe5d5aa96f23280de7d916500e94f8c4f9a
dslyvka <daniilslyvka@gmail.com>
2022-06-02 17:41:18 +0300

    structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}.`);
+});
+//   .catch(error => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
